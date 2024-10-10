031409363cd69e9e129de26b77ee4b36c2e192ab
omkar3720 <omkarshinde2728@gmail.com>
2024-10-10 18:09:33 +0530

    add

--- demoproject/src/demoproject/Test.java
+++ demoproject/src/demoproject/Test.java
@@ -6,6 +6,10 @@ public class Test
 	{
 		System.out.println("m1-Method");
 	}
+	public void m2()
+	{
+		System.out.println("m2-Method");
+	}
    public static void main(String[] args) 
    {
 	   Test t = new Test();
